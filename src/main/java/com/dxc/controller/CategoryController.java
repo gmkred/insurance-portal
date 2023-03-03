b2758ebd1d69d7fcf46a59fd8ac03f026a31ca44
Aashish Patil <aashish.patil@dxc.com>
2023-03-03 12:04:03 +0530

    my_commits

--- src/main/java/com/dxc/controller/CategoryController.java
+++ src/main/java/com/dxc/controller/CategoryController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.dxc.dto.CategoryDTO;
 import com.dxc.model.Category;
 import com.dxc.service.ICategoryService;
@@ -19,27 +21,66 @@ import com.dxc.utils.ConversionUtils;
 public class CategoryController {
 	@Autowired
 	private ICategoryService service;
-
+	
+	/**
+	 * 
+	 * @return
+	 */
 	@GetMapping("/")
 	public String showCategoryPage() {
 		return "category";
 	}
-
+	/**
+	 * This method is responsible for insertion or creation of a 
+	 * category record in the database.
+	 * @param categoryName parameter stores the ModelAttribute in it from UI page
+	 * and @ModelAttribute maps UI page ModelAttribute object to the Java object.
+	 * @param model is used here to send the data from controller to UI
+	 * @return "category" page which is a string
+	 */
 	@PostMapping("/save")
-	public String saveCategory(@ModelAttribute(name = "categoryName") String categoryName, Model model) {
+	public String saveCategory(
+			@ModelAttribute(name = "categoryName") String categoryName, 
+			Model model) {
 		Long id = service.saveCategory(categoryName);
 		String message = "Category created with id : " + id;
 		model.addAttribute("message", message);
 		return "category";
 	}
-
+	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/details")
 	public String getCategories(Model model) {
 		List<Category> categories = service.getCategories();
-		System.err.println("no of categories present in the list : " + categories.size());
 		List<CategoryDTO> categoryDTOs = ConversionUtils.convertCategoryToDTO(categories);
 		model.addAttribute("categories", categoryDTOs);
 		return "categoryDetails";
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/delete")
+	public String deleteCategory(
+			@RequestParam Long id,
+			Model model
+			) {
+		service.deleteCategory(id);
+		String message = new StringBuffer().append("Category having id ")
+				.append(id)
+				.append(" is deleted successfully")
+				.toString();
+		
+		model.addAttribute("message", message);
+		return "redirect:details";
+		
 	}
 
 }
--- src/main/java/com/dxc/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/dxc/service/impl/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dxc.exceptions.CategoryNotFoundException;
 import com.dxc.model.Category;
 import com.dxc.repo.CategoryRepository;
 import com.dxc.service.ICategoryService;
@@ -24,5 +25,14 @@ public class CategoryServiceImpl implements ICategoryService {
 	@Override
 	public List<Category> getCategories() {
 		return categoryRepo.findAll();
+	}
+
+	@Override
+	public void deleteCategory(Long id) {
+		Category category = categoryRepo.findById(id)
+				.orElseThrow(() -> new CategoryNotFoundException("This category with id: "+id+" not exist"));
+		
+		categoryRepo.delete(category);
+		
 	}
 }
